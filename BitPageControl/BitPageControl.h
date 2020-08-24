d808dd71f68e3e542e56fceaaf8ce3575c68f2c7
Shahriyar <sh.soheytizadeh@gmail.com>
2020-08-24 22:27:06 +0200

    Initial project setup

---
+++ BitPageControl/BitPageControl.h
@@ -0,0 +1,19 @@
+//
+//  BitPageControl.h
+//  BitPageControl
+//
+//  Created by Wise Sapien on 24/08/2020.
+//  Copyright © 2020 Shahriar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BitPageControl.
+FOUNDATION_EXPORT double BitPageControlVersionNumber;
+
+//! Project version string for BitPageControl.
+FOUNDATION_EXPORT const unsigned char BitPageControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BitPageControl/PublicHeader.h>
+
+
---
+++ BitPageControlDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BitPageControlDemo
+//
+//  Created by Wise Sapien on 24/08/2020.
+//  Copyright © 2020 Shahriar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
